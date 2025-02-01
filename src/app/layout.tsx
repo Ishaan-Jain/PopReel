0a1699b9d8441f816b4ad1e715dce3b9e54a6649
Ishaan Jain <ishaanj@Ishaans-MacBook-Air.local>
2025-02-01 14:25:41 -0500

    added clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
-import "./globals.css";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+          </header>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
